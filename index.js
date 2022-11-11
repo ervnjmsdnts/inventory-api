0c7197f91b89e50d1c8c95332ca4e078f0874609
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-11-11 19:58:59 +0800

    Cors header

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const main = async () => {
 
   app.use(
     cors({
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+      origin: true,
       credentials: true
     })
   )
